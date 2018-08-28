c0b62ee64ea1585f4360bbc38ed152ff6625b6ab
americanman <snowboardracer7@gmail.com>
2018-08-27 22:17:08 -0400

    Add MySQL methods

--- express.js
+++ express.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const port = 8999
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-const mysql = require('mysql')
+const mysql = require('mysql');
+app.use(cors());
+app.use(bodyParser.json());
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -13,14 +18,29 @@ const connection = mysql.createConnection({
 app.get('/api', (req,res) => res.json('You are the world'))
 
 app.get('/api/locations', (req,res) => {
-  connection.connect()
   connection.query('SELECT * FROM newLocation', (err, rows, filds) => {
     if(err) throw err
     res.json(rows)
-    connection.end()
   })
 })
 
+app.delete('/api/dellocations/:id', (req,res) => {
+  const id = req.params.id
+  connection.query('DELETE FROM newLocation WHERE id=?', [id], (err, rows, filds) => {
+    if(err) throw err
+    console.log('here')
+    res.json(rows)
+  })
+})
+
+
+app.post('/api/addlocations', (req,res) => {
+  console.log(req.body)
+  connection.query('INSERT INTO newLocation (Name, Lat, Lng) VALUES(?,?,?)', [req.body.name, req.body.location.lat, req.body.location.lng], (err, rows, filds) => {
+    if(err) throw err
+    res.json(rows)
+  })
+})
 
 app.listen(port)
 
